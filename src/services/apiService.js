b084da7c5a414334d3c0927ae794c6461c2aee47
Hisada-VI <hisada.vi@gmail.com>
2024-03-13 17:13:53 -0600

    Step C

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,37 +1,61 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://dummyjson.com/products';
-export const fetchProducts = async  () => {
+
+export const fetchProducts = async () => {
     try {
         const response = await axios.get(BASE_URL);
         return response.data;
     } catch (error) {
+        console.error("Error fetching products", error);
         throw error;
     }
 };
 
-export const getProductDetails = async  (id) => {
+export const getProductDetails = async (id) => {
     try {
-        const response = await axios.get(id);
+        const response = await axios.get(`${BASE_URL}/${id}`);
         return response.data;
     } catch (error) {
+        console.error("Error fetching product details", error);
         throw error;
     }
 };
 
 export const removeProduct = async (id) => {
     try {
-        const response= await axios.delete(`${BASE_URL}/${id}`);
+        const response = await axios.delete(`${BASE_URL}/${id}`);
         return response.data;
     } catch (error) {
+        console.error("Error in removing product", error);
         throw error;
     }
 }
-export const addProduct = ( product ) => {
-    return axios.post(`${BASE_URL}/add`, JSON.stringify(product));
-    };
 
-export const editProduct = (id, product) => {
-    return axios.put(`${BASE_URL}/add`, JSON.stringify(product));
-}
+export const addProduct = async (product) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/add`, product, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error adding product", error);
+        throw error;
+    }
+};
 
+export const editProduct = async (id, product) => {
+    try {
+        const response = await axios.put(`${BASE_URL}/${id}`, product, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error("Error editing product", error);
+        throw error;
+    }
+};
